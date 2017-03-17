c7a66cfc0735f1a90475945610f65ed011b974f2
z2c-akok <akok@L400LT510.dhs.net>
2017-03-16 16:01:25 -1000

    ok

--- Chapter07/controller/restapi/router.js
+++ Chapter07/controller/restapi/router.js
@@ -16,7 +16,7 @@
 var express = require('express');
 var router = express.Router();
 var speech_to_text = require('./features/speech_to_text');
-var classifier = require('./features/classifier');
+//var classifier = require('./features/classifier');
 var auth = require('./features/authenticate');
 
 module.exports = router;
@@ -25,7 +25,7 @@ router.get('/api/speech-to-text/token*',speech_to_text.stt_token);
 router.get('/api/text-to-speech/synthesize*',speech_to_text.tts_synthesize);
 
 // classify using NLC
-router.post('/api/understand/classifyInd*', classifier.classifyInd);
+//router.post('/api/understand/classifyInd*', classifier.classifyInd);
 
 router.post('/auth/authenticate*', auth.authenticate);
 router.post('/auth/register*', auth.register);
